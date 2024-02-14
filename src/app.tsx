dc75c6d7512f5a100e4e0a0b49f88f65f8bfde4c
Sergio Silva <sergiobtos@hotmail.com>
2024-02-13 22:30:18 -0800

    config: Tailwind configured

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import logo from './assets/logo-nlw-expert.svg'
+
+export function App() {
+
+  return (
+    <>
+      <img src={logo} />
+      <input type='text' placeholder='Search in your notes...' />
+    </>
+  )
+}
