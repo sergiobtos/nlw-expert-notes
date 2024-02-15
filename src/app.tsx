7ecf527f7c51c6cfee0395cd96cf79b5b19feacc
Sergio Silva <sergiobtos@hotmail.com>
2024-02-14 16:10:20 -0800

    feat: Note-Card and new note card created

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,30 @@
 import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
 
 export function App() {
 
   return (
-    <>
-      <img src={logo} />
-      <input type='text' placeholder='Search in your notes...' />
-    </>
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
+      <img src={logo} alt="NLW Expert" />
+      <form className="w-full">
+        <input
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+          type='text'
+          placeholder='Search in your notes...'
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
   )
 }
