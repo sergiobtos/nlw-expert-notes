5a9f3aa48d73fda27efeefa064f0bd3bec77d6b3
Sergio Silva <sergiobtos@hotmail.com>
2024-02-17 18:01:36 -0800

    conf: new-note-card accepting text input

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {App} from './app'
+import { App } from './app'
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+     <Toaster richColors />
   </React.StrictMode>,
 )
